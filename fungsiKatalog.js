5d3303f8856a83c476941c87e1cd0009124eb411
FirstyanAgeng <firztyan@gmail.com>
2023-01-22 16:48:18 +0700

    fix

---
+++ fungsiKatalog.js
@@ -0,0 +1,14 @@
+const description = document.getElementById("deskripsiProduk");
+const nutrition = document.getElementById("nutrisiProduk");
+
+function katalog() {
+  nutrition.style.display = "block";
+  description.style.display = "none";
+}
+
+function katalog2() {
+  nutrition.style.display = "none";
+  description.style.display = "block";
+}
+
+console.log("testing");
